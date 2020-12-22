9066919c10a2b405374f3fe07fa0a3474db4d6b3
KirillBabenko <3la4ka@gmail.com>
2020-12-23 01:40:35 +0200

    create navigation_graph

---
+++ app/src/main/java/com/dstarlab/notes/screens/NoteFragment.kt
@@ -0,0 +1,20 @@
+package com.dstarlab.notes.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dstarlab.notes.R
+
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+}
