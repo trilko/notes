5b3266b6f2c30723dc4f01b325038da1bd16ed40
KirillBabenko <3la4ka@gmail.com>
2020-12-23 02:18:47 +0200

    create main adapter

---
+++ app/src/main/java/com/dstarlab/notes/screens/main/MainAdapter.kt
@@ -0,0 +1,37 @@
+package com.dstarlab.notes.screens.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.dstarlab.notes.R
+import com.dstarlab.notes.screens.AppNote
+import kotlinx.android.synthetic.main.note_item.view.*
+
+class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    private var mListNotes = emptyList<AppNote>()
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val nameNote: TextView = view.item_note_name
+        val textNote: TextView = view.item_note_text
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nameNote.text = mListNotes[position].name
+        holder.textNote.text = mListNotes[position].text
+    }
+
+    override fun getItemCount(): Int = mListNotes.size
+
+    fun setListNotes(list: List<AppNote>) {
+        mListNotes = list
+        notifyDataSetChanged()
+    }
+}
